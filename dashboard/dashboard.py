6ae38e5b2f0117220eb4f7c3f13bc6c8ca587fa7
arielgibranu <arieleuy18@gmail.com>
2024-12-15 09:52:20 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -24,6 +24,7 @@ with st.sidebar:
     st.header("Selamat Datang")
     st.text("Penyewaan Sepeda")
     st.image("dashboard/sepeda.jpg")
+    st.caption('Ariel Gibranu 2024')
 
 
 
@@ -78,5 +79,3 @@ for p in ax.patches:
                 textcoords='offset points')
 
 st.pyplot(plt)
-
-st.caption('Ariel Gibranu 2024')
